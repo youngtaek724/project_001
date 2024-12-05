545aa674272efae597f9d7d9c407f4f7d065630c
9438190 <9438190@ict-companion.com>
2024-12-05 10:18:15 +0900

    프로젝트 기본 구조 커밋001

---
+++ src/main/java/com/example/project_001/Project001Application.java
@@ -0,0 +1,13 @@
+package com.example.project_001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project001Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_001/Project001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
